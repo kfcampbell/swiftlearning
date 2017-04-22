ffe54ed63486681ca9a8b19369cfbf7b2b5267aa
kfcampbell <keeg4n.campbell@gmail.com>
2017-04-22 07:23:36 -0700

    First commit of tutorial weather bar app.

---
+++ MenuBarWeather/MenuBarWeather/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  MenuBarWeather
+//
+//  Created by Keegan Campbell on 4/22/17.
+//  Copyright © 2017 Keegan Campbell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        statusItem.title = "WeatherBar"
+        statusItem.menu = statusMenu
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
